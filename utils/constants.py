7faecd328ace7dce49e9d8400af6432e9181a840
Kushal Pendekanti <pendekanti.s@northeastern.edu>
2024-11-10 13:30:31 -0500

    utils file changed

--- utils/constants.py
+++ utils/constants.py
@@ -1,13 +1,15 @@
-database_initialize_order = ["database_initializer\ims_initializer.sql"]
+import os
+database_initialize_order = [os.path.join("database_initializer", "ims_initializer.sql")]
+
 
 table_initialize_order = [
-    "table_initializer\company_initializer.sql",
-    "table_initializer\customer_initializer.sql",
-    "table_initializer\employee_initializer.sql",
-    "table_initializer\\brand_initializer.sql",
-    "table_initializer\product_catalog_initializer.sql",
-    "table_initializer\invoice_initializer.sql",
-    "table_initializer\product_initializer.sql",
-    "table_initializer\invoice_line_items_initializer.sql",
-    "table_initializer\processes_initializer.sql",
+    os.path.join("table_initializer", "company_initializer.sql"),
+    os.path.join("table_initializer", "customer_initializer.sql"),
+    os.path.join("table_initializer", "employee_initializer.sql"),
+    os.path.join("table_initializer", "brand_initializer.sql"),
+    os.path.join("table_initializer", "product_catalog_initializer.sql"),
+    os.path.join("table_initializer", "invoice_initializer.sql"),
+    os.path.join("table_initializer", "product_initializer.sql"),
+    os.path.join("table_initializer", "invoice_line_items_initializer.sql"),
+    os.path.join("table_initializer", "processes_initializer.sql"),
 ]
